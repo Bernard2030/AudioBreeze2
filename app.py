01e0d22920fe470909d71c8049fb2705b8f462df
Bernard Opiyo <brobernard.254@gmail.com>
2021-09-28 14:42:51 +0300

    add tables

--- app.py
+++ app.py
@@ -15,3 +15,24 @@ app=Flask(__name__)
 
 app.config['SECRET_KEY']= 'A23bxsa7188sjksdc8u2cxcse23'
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://moringa:Access@localhost/audiobreeze2'
+
+# calling the db
+db= SQLAlchemy(app)
+
+# tables for user and audio
+
+class User(db.Model):
+  id=db.Column(db.Integer, primary_key=True)
+  public_id=db.Column(db.String(250), unique=True)
+  password = db.Column(db.String(250))
+  name=db.Column(db.String(200))
+  admin=db.Column(db.Boolean)
+
+class Audio(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    songName=db.Column(db.String(100), unique=True)
+    artistName=db.Column(db.String(50))
+    songDuration=db.Column(db.Integer)
+    songType=db.Column(db.String(50))
+    songDescription=db.Column(db.String(100))
+    songQuality=db.Column(db.String(100))
