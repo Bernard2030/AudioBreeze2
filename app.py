083cd6633b061c03837261772c7fa65ad73db411
Bernard Opiyo <brobernard.254@gmail.com>
2021-09-28 14:38:45 +0300

    add application initialization

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ import uuid
 from werkzeug.security import generate_password_hash,check_password_hash
 import datetime
 from functools import wraps
+
+
+# initializing the application
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY']= 'A23bxsa7188sjksdc8u2cxcse23'
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://moringa:Access@localhost/audiobreeze2'
